f5a6ed549f3afe851e560a5485f4159441b4f7a4
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-02-11 01:41:34 +0530

    added new array to jobcard

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function JobCard() {
+  const skills = ["Javascript", "ReactJs", "NodeJs", "GraphQL"];
   return (
     <div>
       <div>
         <div>
           <h1>Frontend Developer</h1>
           <p> Full time &#x2022; Fresher &#x2022; In-Office</p>
+
+          <div>
+            {skills.map((skilld) => {
+              <p>{skilld}</p>;
+            })}
+          </div>
         </div>
       </div>
     </div>
